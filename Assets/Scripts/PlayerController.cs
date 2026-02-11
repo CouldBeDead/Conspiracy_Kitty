4febe629e0190c9f2ed3fda9f5a28f659f389255
ThatGuy06 <dinoljb@gmail.com>
2026-02-11 13:24:28 -0700

    Movement and physics working
    -Player can move from left to right and jump
    -"No Friction" physics material ensures the player can't get stuck onto walls
    -Flip() method doesn't work as intended

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,49 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private float horizontal;
+    private float jumpingPower = 16f;
+    private int speed = 8;
+    private bool isFacingRight = true;
 
-    // Update is called once per frame
+    [SerializeField] private Rigidbody2D rB;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
     void Update()
     {
-        
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if(Input.GetButtonDown("Jump") && isGrounded())
+        {
+            rB.linearVelocity = new Vector2(rB.linearVelocity.x, jumpingPower); 
+        }
+
+        if(Input.GetButtonDown("Jump") && rB.linearVelocity.y > 0f)
+        {
+            rB.linearVelocity = new Vector2(rB.linearVelocity.x, rB.linearVelocity.y * 0.5f);
+        }
+
+        //Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        rB.linearVelocity = new Vector2(horizontal * speed, rB.linearVelocity.y);
+    }
+
+    private bool isGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+    private void Flip()
+    { 
+        if(isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
     }
 }
