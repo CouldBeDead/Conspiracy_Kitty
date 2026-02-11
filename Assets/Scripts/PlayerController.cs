bbbb4dfce97569c7a1e58886530dfd0b3954d65a
ThatGuy06 <dinoljb@gmail.com>
2026-02-11 14:33:10 -0700

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,12 +15,12 @@ public class PlayerController : MonoBehaviour
     {
         horizontal = Input.GetAxisRaw("Horizontal");
 
-        if(Input.GetButtonDown("Jump") && isGrounded())
+        if(Input.GetButtonDown("Jump") && isGrounded()) //Player jumps if they're on the ground and press the jump key.
         {
             rB.linearVelocity = new Vector2(rB.linearVelocity.x, jumpingPower); 
         }
 
-        if(Input.GetButtonDown("Jump") && rB.linearVelocity.y > 0f)
+        if(Input.GetButtonDown("Jump") && rB.linearVelocity.y > 0f) 
         {
             rB.linearVelocity = new Vector2(rB.linearVelocity.x, rB.linearVelocity.y * 0.5f);
         }
@@ -30,14 +30,14 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rB.linearVelocity = new Vector2(horizontal * speed, rB.linearVelocity.y);
+        rB.linearVelocity = new Vector2(horizontal * speed, rB.linearVelocity.y); //Basic left and right movement.
     }
 
-    private bool isGrounded()
+    private bool isGrounded() //Checks if the player has hit the ground or not.
     {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer); 
     }
-    private void Flip()
+    private void Flip() //Doesn't really work. Just teleports the player to the opposite side of the scene.
     { 
         if(isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
         {
