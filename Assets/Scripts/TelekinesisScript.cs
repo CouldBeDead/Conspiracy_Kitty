b37934cb385289f5962b507ad6f6e4448fd1abb1
ThatGuy06 <dinoljb@gmail.com>
2026-02-12 10:38:00 -0700

    Object follows mouse
    -Object follows mouse once the player collides with the object and presses E.
    
    -Need to add the ability to throw objects and drop objects.

--- Assets/Scripts/TelekinesisScript.cs
+++ Assets/Scripts/TelekinesisScript.cs
@@ -1,16 +1,62 @@
 using UnityEngine;
 
-public class telekinesisScript : MonoBehaviour
+public class TelekinesisScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private bool inScene;
+    private bool isColliding;
+    private bool hasAnObject;
+
+    [SerializeField] private float maxSpeed = 10f;
+
+    private Camera mainCamera;
+    private BoxCollider2D boxCollider2D;
+    private GameObject otherObject;
+
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        
+        if(other.gameObject.tag == "Moveable")
+        {
+            isColliding = true;
+
+            //boxCollider2D = other.gameObject.GetComponent<BoxCollider2D>();
+
+            otherObject = other.gameObject;
+        }
+    }
+    private void OnCollisionExit()
+    {
+        isColliding = false;
     }
 
-    // Update is called once per frame
+    private void interactingObjects()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (isColliding == true)
+        {
+            if ((Input.GetKeyDown(KeyCode.E) && hasAnObject == false))
+            {
+                hasAnObject = true;
+                //otherObject.transform.position = Vector2.MoveTowards(otherObject.transform.position, mousePosition, moveSpeed * Time.deltaTime);
+            }        
+        }
+
+        if(hasAnObject == true)
+        {
+            otherObject.transform.position = mouseWorldPosition();
+        }
+    }
+    private Vector2 mouseWorldPosition()
+    {
+        return mainCamera.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
     void Update()
     {
-        
+        interactingObjects();
     }
 }
