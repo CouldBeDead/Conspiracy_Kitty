b6bdda749d4a6a622a7f0fb7ef63a2a99b0f3ed2
Timothy E. <tehrlichman@gmail.com>
2026-02-16 13:07:40 -0700

    Sounds :)

--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class Death : MonoBehaviour
 {
+    private AudioSource audioSource;
+    public AudioClip death;
     private Vector2 respawnPoint;
 
     void Start() 
     {
+        audioSource = GetComponent<AudioSource>();
         respawnPoint = transform.position; // Set the initial respawn point to the starting position
     }
 
@@ -13,6 +16,7 @@ public class Death : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
+            audioSource.PlayOneShot(death); 
             transform.position = respawnPoint; // Respawn the player at the respawn point
         }
     }
