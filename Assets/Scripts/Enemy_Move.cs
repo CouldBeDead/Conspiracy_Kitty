7a389fccc4a5510866e760a2228ecbba8017d97c
HofferIsaiah-ux <hoffer.isaiah11@gmail.com>
2026-02-16 19:39:20 -0700

    Fixed Enemies Walking Off Edges

--- Assets/Scripts/Enemy_Move.cs
+++ Assets/Scripts/Enemy_Move.cs
@@ -12,6 +12,11 @@ public class EnemyFollow2D : MonoBehaviour
 
     private Rigidbody2D rb;
 
+    //Variables for Ground Checking 
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -25,19 +30,53 @@ public class EnemyFollow2D : MonoBehaviour
             return;
         }
 
-        Vector2 direction = player.position - transform.position;
+        //Vector2 direction = player.position - transform.position;
+        //float distance = direction.magnitude;
+
+        // MOVEMENT LOGIC 
+        Vector2 direction = (Vector2)player.position - rb.position;
         float distance = direction.magnitude;
+
+        //Check if there is NO ground in front of the enemy, if so, stop moving
+        //RaycastHit2D groundInfo = Physics2D.Raycast(groundCheck.position, Vector2.down, 1f, groundLayer);
+        bool isGroundAhead = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+
+        Debug.DrawRay(groundCheck.position, Vector2.down * 100f, Color.red, 5.0f); // Visualize the raycast in the editor
+
 
         if (distance > stoppingDistance)
         {
-            direction.Normalize();
-            rb.linearVelocity = direction * moveSpeed;
+            if (isGroundAhead) {
+
+                direction.Normalize();
+
+                // We only move on the X axis to prevent "flying" toward a jumping player
+                rb.linearVelocity = new Vector2(direction.x * moveSpeed, rb.linearVelocity.y);
+                
+                // FLIP BASED ON DIRECTION
+                HandleFlip(direction.x);
+            }
+            else 
+            {
+                rb.linearVelocity = new Vector2(0, rb.linearVelocity.y);
+                
+                HandleFlip(direction.x); // Still flip even if we can't move forward
+            }
         }
         else
         {
-            // Fully stop and prevent drift
-            rb.linearVelocity = Vector2.zero;
+            rb.linearVelocity = new Vector2(0, rb.linearVelocity.y);
             rb.angularVelocity = 0f;
+
         }
+    }
+
+    // Flip logic
+    void HandleFlip(float moveX)
+    {
+        if (moveX > 0.1f)
+            transform.eulerAngles = new Vector3(0, 0, 0); // Face Right
+        else if (moveX < -0.1f)
+            transform.eulerAngles = new Vector3(0, 180, 0); // Face Left
     }
 }
